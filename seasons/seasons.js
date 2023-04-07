6dca64bfaedca76c4d6e8e9a2badd87c23745a14
xueqian <justin9011730@gmail.com>
2023-04-07 15:26:08 +0800

    第九次提交

---
+++ seasons/seasons.js
@@ -0,0 +1,30 @@
+
+
+const bg1button = document.querySelector('#bg1button');
+const bg2button = document.querySelector('#bg2button');
+let body = document.getElementsByTagName("body")[0];
+
+
+
+bg1button.addEventListener('click', function () {
+    body.classList.add("bg1class");
+});
+
+bg2button.addEventListener('click', function () {
+    body.classList.add("bg2class");
+});
+
+bg3button.addEventListener('click', function () {
+    body.classList.add("bg3class");
+});
+
+bg4button.addEventListener('click', function () {
+    body.classList.add("bg4class");
+});
+
+function removeAllClasses() {
+    var elements = document.querySelectorAll("body");
+    elements.forEach(function (el) {
+        el.removeAttribute("class");
+    });
+}
